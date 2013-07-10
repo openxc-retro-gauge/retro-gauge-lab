ad1bfd54df10d47966dea257bc027c5cf4325cb9
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-07-10 15:42:39 -0400

    Refactor organization into 3 sections.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
-$(document).ready(function() {
+var highlightActiveSection = function() {
     $('.nav li').each(function(i, value) {
-        if($(value).find("a").attr("href") === window.location.pathname) {
+        var href = $(value).find("a").attr("href");
+        if(href === window.location.pathname || href === window.location.hash) {
             $(value).addClass('active');
         }
     });
+};
+
+$(document).ready(function() {
+    highlightActiveSection()
+    $('.nav li').click(function(event) {
+        $('.nav-list li.active').each(function(i, value) {
+            $(value).removeClass('active');
+        });
+        $(event.target).parent().addClass('active');
+    });
 });
+
